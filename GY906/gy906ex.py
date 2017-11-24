454680191fd82b05d0e6867003513f0e5d9d0591
aid402 <aid402@gmail.com>
2017-11-24 21:00:20 +0700

    Create gy906ex.py

---
+++ GY906/gy906ex.py
@@ -0,0 +1,9 @@
+import time
+import mlx90614
+from machine import I2C, Pin
+i2c = I2C(scl=Pin(5), sda=Pin(4))
+sensor = mlx90614.MLX90614(i2c)
+
+while True:
+	print(sensor.read_ambient_temp(), sensor.read_object_temp())
+	time.sleep_ms(500)
